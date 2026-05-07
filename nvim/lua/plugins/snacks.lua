81a73224f0d2ac92aaba72121fd42e24a09a64da
Stephen Renwick <stephen.renwick@omicronenergy.com>
2026-05-07 09:12:10 +0200

    updated nvim config

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          -- Show hidden/ignored files in the explorer (<leader>e)
+          explorer = {
+            hidden = true,
+            ignored = true,
+          },
+          -- Show hidden/ignored files in the file finder (<leader><leader>)
+          files = {
+            hidden = true,
+            ignored = true,
+          },
+          -- Also apply to grep (<leader>/)
+          grep = {
+            hidden = true,
+            ignored = true,
+          },
+        },
+      },
+    },
+  },
+}
